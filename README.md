1c521638d3637d670dd1016b8064810a7dcabea9
Shyam Habarakada <shyam.habarakada@gmail.com>
2013-06-28 18:39:41 -0700

    Initial draft and usage

--- README.md
+++ README.md
@@ -2,3 +2,63 @@ js-virtual-list-view
 ====================
 
 A generic HTML component for rendering very large lists. Supports variable height elements. Currently depends on jQuery and _ (underscore) and we can consider refactoring those dependencies based on demand. Collaborators are needed. For example, I am not planning to add touch support for the scroll gesture until I need it, but someone else might want to do that.
+
+Usage:
+
+Assuming you have a document with container for the large list like,
+
+    <div id="listContainer"></div>
+
+And CSS like,
+
+    BODY { padding: 40px; }
+
+    #listContainer {
+        width: 200px;
+        height: 500px;   
+        border: 1px dotted #fcc;
+    }
+
+The following javascript will pupulate the list with test data:
+
+    // test data
+    var testData = [],
+        selected = [],
+        bgcolors = ['#fcc','#ffc','#cff','#cfc'],
+        size = 200;
+
+    for(var i=0; i<size; i++) {
+        testData.push({ id: i, label: "list item " + i, selectable: true });
+    }
+
+    // test data source
+    var listDataSource = {
+        /* contentSource methods */
+        contentForRowAtIndex     :  function getContentForItem(index) {
+                                        var randomHeight = 20 + Math.round(50 * Math.random()),
+                                            randomBackground = bgcolors[Math.round(4 * Math.random())],
+                                            e = $('<div class="vlv-content"' +
+                                                  'style="height:' + randomHeight + 
+                                                  'px;font-family:Helvetica;padding:5px;background-color:' +
+                                                  randomBackground + ';"></div>')[0];
+                                        e.textContent = testData[index].label;
+                                        return e;
+                                    },
+        
+        numberOfRows             :  function() { return testData.length; },
+
+        /* delegate methods */
+        onSelectRow              : function(index, element) { 
+                                        selected[index] = true;
+                                        element.innerHTML += " (clicked)";
+                                        element.style.backgroundColor = "#f66";
+                                   }
+    }
+
+    var listContainer = $('#listContainer')[0],
+        vlv = new jsvlv($(listContainer).width(), 
+                        $(listContainer).height(), 
+                        listDataSource, 
+                        listDataSource /* is also the delegate */);
+
+    vlv.show($('#listContainer')[0]);
---
+++ vlv.js
@@ -0,0 +1,154 @@
+function jsvlv(width,height,contentSource,delegate) {
+    
+    var _i = this,   
+        _el = null,
+        _elId = ("vlv" + Math.round(Math.random() * 1000000)), 
+        _t = '<div id="<%= id %>" class="vlv-container"' +
+             ' style="width:<%=width %>px;height:<%=height %>px;"></div>',
+        _$el = $(_.template(_t, { id: _elId, width: width, height: height })),
+        _el = _$el[0],
+        _content = $('<div class="vlv-content"></div>')[0],          
+        _viewportItems = [],
+        _viewportStartIndex = 0,
+        _viewportLastIndex = -1,
+        _contentHeight = 0,
+        _scrollDistance = 0,
+        _numberOfRows = contentSource ? contentSource.numberOfRows() : 0;
+        
+    function onItemClick(index,element) {
+        // todo: add support for element outerHeight changing after this event
+        delegate.onSelectRow(index,element);
+    }
+    
+    function addContentClickHandler(index,element) {
+        element.addEventListener("click", function(e){ onItemClick(index,element); }, false);        
+    }
+
+    function removeContentClickHandler(index,element) {
+        element.removeEventListener("click", function(e){ onItemClick(index,element); }, false);        
+    }
+        
+    function scroll() {
+        var dy = _scrollDistance,
+            index;
+        _content.style.top = dy + "px";
+        if(dy < 0) {
+            index = _viewportEndIndex + 1;
+            while(height - dy - _contentHeight > 0) {
+                if(index < _numberOfRows) {
+                    push(index); 
+                    index++;
+                } else {
+                    _scrollDistance = height - _contentHeight;
+                    _content.style.top = _scrollDistance + "px";
+                    break;
+                }
+                _i.dbgdmp();
+            }
+            // todo: trim the top
+        } else {
+            if(_scrollDistance > 0) {
+                _content.style.top = "0px";
+                _scrollDistance = 0;
+            }
+            // todo: trim the bottom and insert back to the top
+        }
+    }
+    
+    function onMouseWheel(e) {
+        e.preventDefault();
+        _scrollDistance += e.wheelDeltaY;
+        requestAnimationFrame(scroll);
+    }
+    
+    // add to bottom
+    function push(index) {
+        var ce = contentSource.contentForRowAtIndex(index),
+            h = $(ce).outerHeight();
+        _contentHeight += h;
+        _viewportItems.push({ index: index, element: ce, height: h });
+        _content.appendChild(ce);
+        addContentClickHandler(index,ce);
+        _viewportEndIndex = index;
+    }
+
+    // insert at top
+    function unshift(index) {
+        var ce = contentSource.contentForRowAtIndex(index),
+            h = $(ce).outerHeight();
+        _contentHeight += h;
+        _viewportItems.unshift({ index: index, element: ce, height: h });
+        _content.insertBefore(ce,_content.firstChild);
+        addContentClickHandler(index,ce);
+        _viewportStartIndex = index;
+    }
+    
+    // remove from bottom
+    function pop() {
+        var elInfo = _viewportItems.pop();
+        removeContentClickHandler(elInfo.index,elInfo.element);
+        _content.removeChild(elInfo.element);
+        _contentHeight -= elementInfo.height;
+        _viewportEndIndex--;
+    }
+
+    // remove from top
+    function shift(contentInfo) {
+        var elInfo = _viewportItems.shift();
+        removeContentClickHandler(elInfo.index,elInfo.element);
+        _content.removeChild(elInfo.element);
+        _contentHeight -= elementInfo.height;
+        _viewportStartIndex++;
+    }
+    
+    function fill() {
+        var ce,h,
+            i = 0;
+        while(_contentHeight < height && i < _numberOfRows) {
+            push(i);
+            i++;
+        };
+    }
+    
+    _i.getElement = function() {
+        return _el;
+    }
+    
+    _i.show = function(container) {
+        _el.appendChild(_content);
+        _el.addEventListener("mousewheel",onMouseWheel,true);
+        container.appendChild(_el);
+        fill();
+    }
+    
+    _i.hide = function() {
+        // todo: cleanup content bindings and elements
+        _el.removeEventListener("mousewheel",onScroll,true);
+        container.removeChild(_el);
+    }
+    
+    _i.reload = function() {
+        while (_content.firstChild) {
+          _content.removeChild(_content.firstChild);
+        }
+        _viewportItems = [];
+        _viewportStartIndex = 0;        
+        _viewportEndIndex = -1;
+        _contentHeight = 0;
+        _scrollDistance = 0;
+        _numberOfRows = contentSource ? contentSource.numberOfRows() : 0;
+        fill();
+    }
+    
+    _i.dbgdmp = function() {
+        console.log({ 
+            content : _viewportItems,
+            scrollDistance: _scrollDistance,
+            contentHeight: _contentHeight,
+            firstIndex: _viewportStartIndex,
+            lastIndex: _viewportEndIndex
+        });
+    }
+
+    return _i;
+}
